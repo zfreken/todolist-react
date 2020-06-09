4187404f75a80dada7dc02819ab2a5066d325991
Zafer EKEN <zafereken@Zafer-MacBook-Air.local>
2020-06-10 00:37:12 +0300

    Updates

---
+++ precache-manifest.aeba4c678d5065a393d84a66fb7692b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df6160a002017dfb4db093e79425302",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "25b05734a05160ebae71",
+    "url": "/todolist-react/static/css/main.59d0ee98.chunk.css"
+  },
+  {
+    "revision": "c48412ea5e2ba53822e3",
+    "url": "/todolist-react/static/js/2.6995409b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-react/static/js/2.6995409b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25b05734a05160ebae71",
+    "url": "/todolist-react/static/js/main.f09de414.chunk.js"
+  },
+  {
+    "revision": "63e102f1ceff877827c7",
+    "url": "/todolist-react/static/js/runtime-main.55c622dd.js"
+  }
+]);
